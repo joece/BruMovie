1f0b9658260fc7ebbec92b7c76f8d1ec94047af7
unknown <834427699@qq.com>
2018-06-27 19:04:09 +0800

    origin ui

--- client/app.js
+++ client/app.js
@@ -3,7 +3,31 @@ var qcloud = require('./vendor/wafer2-client-sdk/index')
 var config = require('./config')
 
 App({
+    globalData:{
+      userInfo: null
+    },
+
     onLaunch: function () {
-        qcloud.setLoginUrl(config.service.loginUrl)
+      var logs = wx.getStorageSync('log') || []
+      logs.unshift(Date.now())
+      wx.setStorageSync('logs', logs)
+      qcloud.setLoginUrl(config.service.loginUrl)
+
+      wx.getSetting({
+        success: res => {
+          if(res.authSetting['scope.userInfo']){
+            wx.getUserInfo({
+              success: res => {
+                this.globalData.userInfo = res.userInfo
+
+                if(this.userInfoReadyCallback){
+                  this.userInfoReadyCallback(res)
+                }
+              }
+            })
+          }
+        }
+      })
     }
+    
 })
