b5a853ee3f200e410520b9b6406a23621983e3d7
unknown <834427699@qq.com>
2018-07-05 23:37:23 +0800

    do sth

--- client/pages/futuretickets/index.js
+++ client/pages/futuretickets/index.js
@@ -5,24 +5,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-   myorders:[
-      {
-        'cinema':'珠江金逸影院（大学城店)',
-        'price': '38',
-        'chairnum': "3号厅-6FL9排13座",
-        'movietime': "07月02日 13:00",
-        'moviename': "金瓶梅",
-        'postpath': "../../images/ticket.png"
+   myorders:[]
+  },
+  onLoad: function(){
+    var that = this
+    wx.request({
+      url: 'https://k3d2hspl.qcloud.la/weapp/getOrder',
+      method: 'POST',
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
       },
-      {
-        'cinema': '珠江金逸影院（zhong城店)',
-        'price': '38',
-        'chairnum': "3号厅-6FL9排13座",
-        'movietime': "07月02日 13:00",
-        'moviename': "sejie",
-        'postpath': "../../images/ticket.png"
-      }
-    ]
+      data: {
+        skey: wx.getStorageSync('weapp_session_' + 'F2C224D4-2BCE-4C64-AF9F-A6D872000D1A')
+      },
+      success(result) {
+        wx.pc = result.data
+        console.log(result)
+
+        }
+     })
   },
 
   showticketorder: function () {
